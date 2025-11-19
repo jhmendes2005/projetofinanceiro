b4d6212c2564c69d359db340984b215025732736
jhmendes2005 <jhmendes2005@gmail.com>
2025-11-19 16:58:17 -0300

    middleware novo supa auth 2

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,13 @@
-// middleware.ts
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+// Agora o import vai funcionar porque o nome bate
+import { updateSession } from '@/lib/supabase/session' 
 
 export async function middleware(request: NextRequest) {
-  // Delegamos toda a lógica para o arquivo auxiliar
-  // Isso evita que o Next.js tente importar dependências de Node.js no Edge
   return await updateSession(request)
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Esta é a forma correta de corrigir erros de dependência no Next.js moderno.
-  // Isso impede que o Next tente empacotar bibliotecas que usam recursos nativos do Node.
   serverExternalPackages: [
     'pino', 
     'winston', 
     'bufferutil', 
     'utf-8-validate', 
-    'canvas',
-    'sharp'
   ],
 }
 
