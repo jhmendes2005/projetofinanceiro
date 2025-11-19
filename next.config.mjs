fd5a3463305fcbeab15fd05d89d19192c8fda4f9
jhmendes2005 <jhmendes2005@gmail.com>
2025-11-19 16:44:10 -0300

    primeiro commit2

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Adicione isto para corrigir o erro de __dirname em dependências
+  serverExternalPackages: ['pino', 'winston', 'bufferutil', 'utf-8-validate'],
+  
+  // Às vezes necessário se o erro persistir:
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
 }
 
 export default nextConfig
